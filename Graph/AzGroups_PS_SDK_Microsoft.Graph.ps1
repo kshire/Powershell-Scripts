6399fedfeb49330cabd5f8ecc4804f8e4a3c2d0a
Kevin Shire <40002967+kshire@users.noreply.github.com>
2022-08-07 22:31:26 -0500

    Update AzGroups_PS_SDK_Microsoft.Graph.ps1

--- Graph/AzGroups_PS_SDK_Microsoft.Graph.ps1
+++ Graph/AzGroups_PS_SDK_Microsoft.Graph.ps1
@@ -16,14 +16,44 @@ Get-MgGroup
 
 # Get a group by DisplayName
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.graph.groups/get-mggroup?view=graph-powershell-1.0#example-2-get-a-group-by-the-display-name
-Get-MgGroup -Filter "DisplayName eq 'test1'"
+Get-MgGroup -Filter "DisplayName eq 'test group 2'" | fl
 
 # Make a new group
+# Put parameters in a hashtable
 $params = @{
-  DisplayName = 'Test Group'
+  DisplayName = 'AZ_ServerSecurity_Barn'
   MailEnabled = $False
-  MailNickName = 'testgroup'
+  MailNickName = 'AZ_ServerSecurity_Barn'
   SecurityEnabled = $true
 }
-New-MgGroup @params
+# splat the parameters to the command
+$group = New-MgGroup @params
 
+
+
+# Add an Owner
+## Useing UPN by ref
+New-MgGroupOwnerByRef -GroupId $group.Id -AdditionalProperties @{"@odata.id"="https://graph.microsoft.com/v1.0/users/kshire@shirekevinjohndeere.onmicrosoft.com"}
+## Using user Id
+New-MgGroupOwnerByRef -GroupId $group.Id -AdditionalProperties @{"@odata.id"="https://graph.microsoft.com/v1.0/users/a0512e69-7815-4685-b916-e9da762a578f"}
+
+
+
+# Add a member
+New-MgGroupMember -GroupId $group.Id -DirectoryObjectId $user.Id
+# Add a member by reference
+New-MgGroupMemberByRef -GroupId $group.Id -AdditionalProperties @{"@odata.id"="https://graph.microsoft.com/v1.0/users/kshire@shirekevinjohndeere.onmicrosoft.com"}
+
+
+
+# Remove a group
+Remove-MgGroup -GroupId $group.Id
+
+
+
+# Get group members
+Get-MgGroupMember -GroupId $group.id
+
+(Get-MgGroup -GroupId $group.Id -ExpandProperty members).members | ForEach-Object { $_.additionalproperties }
+(Get-MgGroup -GroupId $group.Id -ExpandProperty members).members | Select-Object -ExpandProperty AdditionalProperties
+Get-MgGroup -GroupId $group.Id -ExpandProperty members | select -ExpandProperty members | select -ExpandProperty additionalproperties
